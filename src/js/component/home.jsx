5677087762cac58d917f06ccb2a3f1638eaf2d1f
Helenagg <hgonzalezg18@gmail.com>
2022-10-21 19:16:23 +0000

    cambio

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -18,9 +18,9 @@ const Home = () => {
 		<>
 			<div className="text-center mt-5 ">
 				<div className="d-grid gap-2 col-2 p-5 mx-auto bg-dark">			
-					<button type="button" className={(color !== 'red' ? 'btn btn-danger rounded-pill' : 'btn btn-danger rounded-pill border-light border-5')} onClick={() => cambiaColor('red')}>Rojo</button>
-					<button type="button" className={(color !== 'yellow' ? 'btn btn-warning rounded-pill' : 'btn btn-warning rounded-pill border-light border-5')} onClick={() => cambiaColor('yellow')}>Amarillo</button>
-					<button type="button" className={(color !== 'green' ? 'btn btn-success rounded-pill' : 'btn btn-success rounded-pill border-light border-5')} onClick={() => cambiaColor('green')}>Verde</button>
+					<button type="button" className={(color !== 'red' ? 'btn btn-danger rounded-pill' : 'btn btn-danger rounded-pill border-light border-5 text-dark')} onClick={() => cambiaColor('red')}>Rojo</button>
+					<button type="button" className={(color !== 'yellow' ? 'btn btn-warning rounded-pill text-white' : 'btn btn-warning rounded-pill border-light border-5')} onClick={() => cambiaColor('yellow')}>Amarillo</button>
+					<button type="button" className={(color !== 'green' ? 'btn btn-success rounded-pill' : 'btn btn-success rounded-pill border-light border-5 text-dark')} onClick={() => cambiaColor('green')}>Verde</button>
 				</div>
 			</div>
 		</>
